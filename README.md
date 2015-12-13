a875bf9ff5cba1f7f5c8b525eb013b8580b27e26
Chiedo <chiedo@chie.do>
2015-12-12 22:38:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+## MoonWalkPHP
+
+Removes trailing indentation in HEREDOC strings and other strings with multiple lines.
+
+### Usage
+
+Without moonwalk:
+
+```php
+function hello() {
+  $y = 0;
+  if(true) {
+  $x = <<<EOT
+<div>
+  <span>OK</span>
+  <div>
+    <div></div>
+  </div>
+</div>
+EOT;
+ }
+```
+
+With moonwalk:
+
+```php
+include "moon-walk.php"
+function hello() {
+  $y = 0;
+  if(true) {
+  $x = <<<EOT
+    <div>
+      <span>OK</span>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  EOT;
+  $x = moonWalk($x);
+ }
+```
---
+++ moon-walk.php
@@ -0,0 +1,18 @@
+<?php
+function moonWalk($x, $leadingSpaces = 0) {
+  // Break up by new lines
+  $lines = explode("\n", $x);
+  // Find how many leading spaces are in the first line
+  $spacesToRemove = strlen($lines[0]) - strlen(trim($lines[0])) - $leadingSpaces;
+
+  // Remove that many leading spaces from the beginning of each string
+  for($x = 0; $x < sizeof($lines); $x++) {
+    // Remove each space
+    $lines[$x] = preg_replace('/\s/', "", $lines[$x], $spacesToRemove);
+  }
+  // Put back into string on seperate lines
+  $x = implode("\r\n", $lines);
+
+  return $x;
+}
+?>
