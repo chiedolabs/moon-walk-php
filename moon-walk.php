656f94695c56d0a374af96da3f599a3384c1d825
Chiedo <chiedo@chie.do>
2015-12-14 10:24:42 -0500

    Fixed function after lame php issue

--- moon-walk.php
+++ moon-walk.php
@@ -1,18 +1,17 @@
 <?php
 function moonWalk($x, $leadingSpaces = 0) {
   // Break up by new lines
-  $lines = explode("\n", $x);
+  $tmp = explode("\n", $x);
+  $lines = array_values(array_filter($tmp,"not_empty"));
+  
   // Find how many leading spaces are in the first line
   $spacesToRemove = strlen($lines[0]) - strlen(ltrim($lines[0])) - $leadingSpaces;
-
   // Remove that many leading spaces from the beginning of each string
   for($x = 0; $x < sizeof($lines); $x++) {
     // Remove each space
     $lines[$x] = preg_replace('/\s/', "", $lines[$x], $spacesToRemove);
   }
   // Put back into string on seperate lines
-  $x = implode("\n", $lines);
-
-  return $x;
+  return implode("\n", $lines);
 }
 ?>
