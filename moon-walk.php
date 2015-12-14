9ac27d25ed8635b58860d645fec264b1703edd4e
Chiedo <chiedo@chie.do>
2015-12-13 20:55:51 -0500

    Switched to Ltrim

--- moon-walk.php
+++ moon-walk.php
@@ -3,7 +3,7 @@ function moonWalk($x, $leadingSpaces = 0) {
   // Break up by new lines
   $lines = explode("\n", $x);
   // Find how many leading spaces are in the first line
-  $spacesToRemove = strlen($lines[0]) - strlen(trim($lines[0])) - $leadingSpaces;
+  $spacesToRemove = strlen($lines[0]) - strlen(ltrim($lines[0])) - $leadingSpaces;
 
   // Remove that many leading spaces from the beginning of each string
   for($x = 0; $x < sizeof($lines); $x++) {
